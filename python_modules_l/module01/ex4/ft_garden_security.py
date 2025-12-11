ae92be31171866d95718e8413f21d9dce6cc5852
Alvaro <aluluengo@gmail.com>
2025-12-11 14:15:08 +0100

    ex4 correct

--- python_modules_l/module01/ex4/ft_garden_security.py
+++ python_modules_l/module01/ex4/ft_garden_security.py
@@ -1,47 +1,47 @@
 class SecurePlant:
 	factory = []
-	errors = []						#In this process we will check if the values are correct. If they are not, we will index the error in here
+	errors = 0						#In this process we will check if the values are correct. If they are not, we will index the error in here
 	plants_in_factory = 0			#So we can display it later on.
+	error_value = 0
 	class Plant:
 		def __init__(self, name, height, age):
 			self.name = name		#Plant's name
-			try:
-				self.height = height	#Plant's height in CM. Here we call the set_height validation function and the get_height function too.
-			except ValueError as e:
-				SecurePlant.errors = SecurePlant.errors + [f"Invalid operation attempted: {e} [REJECTED]\nSecurity: Negative height rejected\n"]
-			try:
-				self.age = age			#Plant's age in days. Here we call the set_age validation function and get_age function too.
-			except ValueError as e:
-				SecurePlant.errors = SecurePlant.errors + [f"Invalid operation attempted: {e} [REJECTED]\nSecurity: Negative age rejected\n"]
-		
+			self.height = height
+			self.age = age
+
 		@property
-		def	height(self):				#Here we define as a property for security, the setter and getter needs this.
+		def height(self):
 			return self.__height
-		
-		@height.setter				 	#The setter is used to make some checks before we assign a value to a
-		def	height(self, value): 		#content. It works with protected data.
+
+		@height.setter
+		def height(self, value):
 			if value < 0:
-				raise ValueError(f"height {value}")		#Here we set the height protecting it, so
-			self.__height = value						#It cant be a negative value.
+				SecurePlant.errors = 1
+				SecurePlant.error_value = value
+				return
+			else:
+				self.__height = value
 
 		@height.getter
-		def	get_height(self):
-			return self.__height		#Here we get the info securely.
+		def height(self):
+			return self.__height
 
 		@property
-		def	age(self):
-			return self.__age			#Then we do the same with the age.
-
-		@age.getter
-		def	get_age(self):
+		def age(self):
 			return self.__age
 		
 		@age.setter
-		def	age(self, value):
+		def age(self, value):
 			if value < 0:
-				raise ValueError(f"age {value}")
-			self.__age = value
-	
+				print(f"Invalid operation attempted: {value} [REJECTED]\n")
+				SecurePlant.errors = 2
+				return
+			else:
+				self.__age = value
+		@age.getter
+		def age(self):
+			return self.__age
+			
 	@classmethod
 	def create_plant(cls, name, height, age):
 		plant = cls.Plant(name, height, age)
@@ -58,8 +58,12 @@ def ft_plant_factory():
 	print("=== Garden Security System ===")
 	for plant in SecurePlant.factory:
 		print("Plant created:", plant.name, "\nHeight updated:", plant.height, "cm [OK]", "\nAge updated:", plant.age, "days [OK]\n")
-	for error in SecurePlant.errors:
-		print(error)
+	if SecurePlant.errors == 1:
+		print(f"Invalid operation attempted:", SecurePlant.error_value, "[REJECTED]")
+		print("Security: Negative height rejected\n")
+	if SecurePlant.errors == 2:
+		print(f"Invalid operation attempted:", SecurePlant.error_value, "[REJECTED]")
+		print("Security: Negative age rejected\n")
 	for plant in SecurePlant.factory:
 		print("Current plant:", plant.name, f"({plant.height}cm", ",", plant.age, "days)")
 
