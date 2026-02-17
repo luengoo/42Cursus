2cbaf1693d449b36ab634663389b90f74b15f0b3
Alvaro <aluluengo@gmail.com>
2026-02-17 13:10:36 +0100

    Garden management done. Starting module03

--- python_modules_l/module02/ex5/ft_garden_management.py
+++ python_modules_l/module02/ex5/ft_garden_management.py
@@ -6,59 +6,80 @@ class GardenManager:
 		pass
 	class SunError(GardenError):
 		pass
-	class AddPlants:
+	class Plant:
 		garden = []
-		water_tank = 10
+		water_tank = 2
 		def	__init__(self, name, water_lvl, sun_lvl):
-			try:
-				if name == "":
-					raise ValueError("Plant name cannot be empty!")
-				if name in self.garden:
-					raise ValueError("This plant is already in the garden!")
-				self.name = name
-				self.water_lvl = water_lvl
-				self.sun_lvl = sun_lvl
-				self.garden.append(name)
-				print(f"Added {name} successfully")
-			except ValueError as e:
-				print(f"Error adding plant: {e}")
-		
-	def	water_plants(self):
+			if name == "":
+				raise ValueError("Plant name cannot be empty!")
+			if name in self.garden:
+				raise ValueError("This plant is already in the garden!")
+			self.name = name
+			self.water_lvl = water_lvl
+			self.sun_lvl = sun_lvl
+			print(f"Added {name} successfully")
+
+	@classmethod
+	def	create_plant(cls, name, water_lvl, sun_lvl):
+		try:
+			plant = cls.Plant(name, water_lvl, sun_lvl)
+			cls.Plant.garden = cls.Plant.garden + [plant]
+			return plant
+		except ValueError as e:
+			print(f"Error adding plant: {e}")
+	
+	@classmethod
+	def	water_plants(cls):
 		print("\nWatering plants...")
 		try:
-			for plant in self.AddPlants.garden:
-				if self.AddPlants.water_tank < 1:
-					raise WaterError(f"Can't water {name}: Water tank level too low!")
-				print(f"Watering {plant} - success")
-				self.AddPlants.water_tank -= 1
-		except self.WaterError:
+			for plants in cls.Plant.garden:
+				if cls.Plant.water_tank < 1:
+					raise cls.WaterError(f"Can't water {plants.name}: Water tank level too low!")
+				print(f"Watering {plants.name} - success")
+				cls.Plant.water_tank -= 1
+		except cls.WaterError:
 			print("Error:")
 		finally:
-			print("Closing watering sistem (cleanup)")
+			print("Closing watering sistem (cleanup)\n")
 
-	def check_plant_health(self):
+	@classmethod
+	def check_plant_health(cls):
 		print("Checking plant health...")
 		try:
-			for plant in self.AddPlants.garden:
+			for plant in cls.Plant.garden:
 				if plant.water_lvl < 1:
-					raise WaterError(f"Water level {plant.water_lvl} is too low (min 1)")
+					raise cls.WaterError(f"Water level {plant.water_lvl} is too low (min 1)")
 				elif plant.water_lvl > 10:
-					raise WaterError(f"Water level {plant.water_lvl} is too high (max 10)")
+					raise cls.WaterError(f"Water level {plant.water_lvl} is too high (max 10)")
 				elif plant.sun_lvl < 1:
-					raise SunError(f"Sun level {plant.sun_lvl} is too low (min 1)")
+					raise cls.SunError(f"Sun level {plant.sun_lvl} is too low (min 1)")
 				elif plant.sun_lvl > 10:
-					raise SunError(f"Sun level {plant.sun_lvl} is too high (max 10)")
-				print(f"{plant}: healthy (water: {plant.water_lvl}, sun: {plant.sun_lvl})")
-		except self.WaterError or self.SunError:
-			print(f"Error checking {plant}:")
+					raise cls.SunError(f"Sun level {plant.sun_lvl} is too high (max 10)")
+				print(f"{plant.name}: healthy (water: {plant.water_lvl}, sun: {plant.sun_lvl})")
+		except (cls.WaterError, cls.SunError) as e:
+   	 		print(f"Error checking {plant.name}: {e}")
+	
+	@classmethod
+	def error_recovery(cls):
+		print("\nTesting error recovery...")
+		try:
+			if cls.Plant.water_tank < 1:
+				cls.Plant.water_tank = 10
+				raise cls.GardenError(f"Not enough water in the tank")
+		except cls.GardenError as e:
+			print(f"Caught GardenError: {e}")
+		finally:
+			print("System recovering and continuing...")
+
 			
 
 def	main_trial():
 	print("Adding plants to garden...")
-	GardenManager.AddPlants("tomato", 4, 7)
-	GardenManager.AddPlants("lettuce", 14, 8)
-	GardenManager.AddPlants("", 14, 8)
-	GardenManager.water_plants(GardenManager)
-	GardenManager.check_plant_health(GardenManager)
+	GardenManager.create_plant("tomato", 4, 7)
+	GardenManager.create_plant("lettuce", 14, 8)
+	GardenManager.create_plant("", 14, 8)
+	GardenManager.water_plants()
+	GardenManager.check_plant_health()
+	GardenManager.error_recovery()
 
 main_trial()
