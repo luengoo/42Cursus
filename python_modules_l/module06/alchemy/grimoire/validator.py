1cf93ce39a6512bd758357f7e90167092b327646
Alvaro Luengo Vega <alluengo@c2r3s3.42madrid.com>
2026-03-10 19:11:25 +0100

    module06 ready again for evaluation. starting with module07

--- python_modules_l/module06/alchemy/grimoire/validator.py
+++ python_modules_l/module06/alchemy/grimoire/validator.py
@@ -4,6 +4,6 @@ VALID_INGREDIENTS = {"fire", "water", "earth", "air"}
 def validate_ingredients(ingredients: str) -> str:
     parts = ingredients.split()
     if all(p in VALID_INGREDIENTS for p in parts):
-        return f"{ingredients} - VALID."
+        return f"{ingredients} - VALID"
     else:
-        return f"{ingredients} - INVALID."
+        return f"{ingredients} - INVALID"
