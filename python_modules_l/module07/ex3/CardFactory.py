8886d198ba5874f1d808299a80d8774fd5a757c4
Alvaro <aluluengo@gmail.com>
2026-03-17 15:57:45 +0100

    ex3 in process

--- python_modules_l/module07/ex3/CardFactory.py
+++ python_modules_l/module07/ex3/CardFactory.py
@@ -1,18 +1,39 @@
 from abc import ABC, abstractmethod
 from ex0.Card import Card
+from ex0.CreatureCard import CreatureCard
+from ex1.SpellCard import SpellCard
+from ex1.ArtifactCard import ArtifactCard
+
 
 class CardFactory(ABC):
     @abstractmethod
     def create_creature(self, name_or_power: str | int | None = None) -> Card:
+        creature = CreatureCard(self.name, self.cost, self.rarity,
+                                self.attack, self.health)
+        return creature
 
     @abstractmethod
     def create_spell(self, name_or_power: str | int | None = None) -> Card:
+        if isinstance(name_or_power, str):
+            spell = SpellCard(self.name, self.cost, self.rarity, name_or_power)
+            return spell
+        else:
+            raise ValueError(
+                "Not valid effect name for the spell: str required!")
 
     @abstractmethod
     def create_artifact(self, name_or_power: str | int | None = None) -> Card:
+        if isinstance(name_or_power, int):
+            artifact = ArtifactCard(self.name, self.cost, self.rarity,
+                                    name_or_power, self.effect)
+            return artifact
+        else:
+            raise ValueError("Not valid durability: int required!")
 
     @abstractmethod
     def create_themed_deck(self, size: int) -> dict:
+        pass
 
     @abstractmethod
     def get_supported_types(self) -> dict:
+        pass
--- python_modules_l/module07/ex3/FantasyCardFactory.py
+++ python_modules_l/module07/ex3/FantasyCardFactory.py
@@ -1,5 +1,47 @@
 from .CardFactory import CardFactory
+from ex0.Card import Card
+from ex0.CreatureCard import CreatureCard
+from ex1.SpellCard import SpellCard
+from ex1.ArtifactCard import ArtifactCard
+
 
 class FantasyCardFactory(CardFactory):
+    factory = []
+    aviable_types = {
+        'creatures': ['dragon', 'goblin'],
+        'spells': ['fireball'],
+        'artifacts': ['mana_ring']
+    }
+
     def __init__(self):
+        super().__init__(self)
+
+    def create_creature(self, name_or_power: str | int | None = None) -> Card:
+        creature = CreatureCard(self.name, self.cost, self.rarity,
+                                self.attack, self.health)
+        FantasyCardFactory.factory.append(creature)
+        return creature
+
+    def create_spell(self, name_or_power: str | int | None = None) -> Card:
+        if isinstance(name_or_power, str):
+            spell = SpellCard(self.name, self.cost, self.rarity, name_or_power)
+            FantasyCardFactory.factory.append(spell)
+            return spell
+        else:
+            raise ValueError(
+                "Not valid effect name for the spell: str required!")
+
+    def create_artifact(self, name_or_power: str | int | None = None) -> Card:
+        if isinstance(name_or_power, int):
+            artifact = ArtifactCard(self.name, self.cost, self.rarity,
+                                    name_or_power, self.effect)
+            FantasyCardFactory.factory.append(artifact)
+            return artifact
+        else:
+            raise ValueError("Not valid durability: int required!")
+        
+    def get_supported_types(self) -> None:
+        print(f"Aviable types: {self.aviable_types}")
+    
+    def create_themed_deck(self, size: int):
         
